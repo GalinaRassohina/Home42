bddd4c2eae9a9ccd71b8db8c085e6f5002a5321a
Galina <gal-rassohina@yandex.ru>
2022-11-27 19:10:23 +0700

    код

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 27: Напишите метод, который принимает на вход число и выдаёт 
+//сумму цифр в числе.452 -> 11 82 -> 10 9012 -> 12
+int SumNumbers(int number)
+{
+    int a = number;
+    int result = 0;
+    while(a > 0)
+    {
+        result += a % 10;
+        a /= 10;        
+    }
+    return result;
+}
+
+Console.WriteLine(SumNumbers(452));
